5ca8d6bbd317c96fdc2999ba16d9a5102f2373ce
Lewis Nixon <161777820+lewis-nixon@users.noreply.github.com>
2024-05-30 21:38:02 +0000

    fix nekoray intel version

--- Casks/nekoray.rb
+++ Casks/nekoray.rb
@@ -5,7 +5,7 @@ cask 'nekoray' do
   
     version "3.26"
     arch arm: "arm64",
-        intel: "x86_64"
+        intel: "amd64"
     sha256 arm: "4F127B99F12A97E070528AD73C52C4CBC70E7929B02B9F7A5B3755B07C2F7DF2",
         intel: "B57080209221658B1A086EE9C01FCA7EDB395E2674261E8E9F8E87EED6387614"
 
