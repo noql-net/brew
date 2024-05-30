b5ea12d690277b82c79cb52954e83644644d2b54
Lewis Nixon <161777820+lewis-nixon@users.noreply.github.com>
2024-05-30 21:51:04 +0000

    update warp-plus

--- Casks/warp-plus.rb
+++ Casks/warp-plus.rb
@@ -3,11 +3,11 @@ cask 'warp-plus' do
     desc "Warp+Psiphon, an anti censorship utility for iran."
     homepage "https://github.com/bepass-org/warp-plus/"
 
-    version "1.2.2"
+    version "1.2.3"
     arch arm: "arm64",
         intel: "amd64"
-    sha256 arm: "63817fe6e43d88eb590dc3df9b002b126de3f47804aa46b21ccf691458fc5683",
-        intel: "2a23ed002e661e679b430841b1301db7b72c454ac0c5ac25274353c18de16623"
+    sha256 arm: "b558818794c26ebcce11e83b28c4c11a331d6d59ea8f64e084573629637b8c2f",
+    intel: "698685cef5accb594e33b3fa7920dc0d85babe377c6bed70c46aa9d8ba58f069"
         
     url "https://github.com/bepass-org/warp-plus/releases/download/v#{version}/warp-plus_darwin-#{arch}.zip",
         verified: "https://github.com/bepass-org/warp-plus/"
