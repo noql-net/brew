10d4491b83b1f354876e2db27d73388b3559b004
Lewis Nixon <161777820+lewis-nixon@users.noreply.github.com>
2024-05-31 15:47:54 +0000

    add oblivion-desktop

---
+++ Casks/oblivion-desktop.rb
@@ -0,0 +1,18 @@
+cask 'oblivion' do
+    name "oblivion"
+    desc "Oblivion Desktop - Unofficial Warp Client for Windows/Mac/Linux."
+    homepage "https://github.com/bepass-org/oblivion-desktop"
+    
+    version "0.10.8"
+    arch arm: "arm64",
+        intel: "x64"
+    sha256 arm: "9ac66502a23b211ec71693bbad510f56d2cb142a3397e57930b7d488ceec1234",
+        intel: "d2f8eb5959e34206ff6b3c9a1ece3e5a55c64c8ae3a0d7fa866387944a18acdd"
+        
+    url "https://github.com/bepass-org/oblivion-desktop/releases/download/v#{version}/oblivion-desktop-mac-#{arch}.zip",
+        verified: "https://github.com/bepass-org/oblivion-desktop"
+  
+    auto_updates true
+
+    app "Oblivion Desktop.app"
+end
